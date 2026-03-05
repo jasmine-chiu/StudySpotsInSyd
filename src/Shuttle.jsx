71610d077faabe488f981c36ab98336165c32454
jasmine chiu <jas@jas-MacBook-Pro.local>
2026-03-05 22:30:56 +1100

    feat: added shuttle page structure

--- src/Shuttle.jsx
+++ src/Shuttle.jsx
@@ -1,11 +1,28 @@
 import Header from "./Header"
 
 const Shuttle = () => {
+
     return (
         <>
             <Header />
             <div className="page">
-                insert scraped data
+                <div>
+                    Next available shuttle:
+                </div>
+                <div>
+                    <div className="next-shuttle-container">
+                        <p className="shuttle-direction">
+                            To Arts & Design Campus (from Main Campus):
+                        </p>
+                        <p className="shuttle-time">{}</p>
+                    </div>
+                    <div className="next-shuttle-container">
+                        <p classNam="shuttle-direction">
+                            To Main Campus (from Arts & Design Campus):
+                        </p>
+                        <p className="shuttle-time">{}</p>
+                    </div>
+                </div>
             </div>
         </>
     )
